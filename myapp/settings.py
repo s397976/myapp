6df12aa4ba7a0b7269ea5763e26cb311f0552b90
Your Name <s3979760@rmit.edu.vn>
2024-04-15 16:41:57 +0700

    add image

--- myapp/settings.py
+++ myapp/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_URL = 'static/',
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
@@ -134,3 +135,7 @@ COMPRESS_ENABLED = True
 STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
                        )
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
