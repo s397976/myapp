4e1bcf9d65c74be8aaab83e1f1bd6881a77b677e
Your Name <s3979760@rmit.edu.vn>
2024-04-15 16:04:02 +0700

    new

--- myapp/settings.py
+++ myapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'compressor',
     'tailwind',
     'theme'
+    ,
 ]
 
 MIDDLEWARE = [
--- myapp/urls.py
+++ myapp/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .views import index
+from myapp import views
  
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path("register/",  views.register)
 ]
